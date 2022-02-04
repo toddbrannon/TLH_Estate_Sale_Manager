ebcec4a2ad4dd259bfe4e0975f751c621cea26a8
Todd Brannon <toddqbrannon@gmail.com>
2022-02-04 13:27:14 -0600

    upgrade landing page design and some corrections to invoice data sources in invoice.ts

--- src/invoice.ts
+++ src/invoice.ts
@@ -11,9 +11,19 @@ export const saleToInvoice = sale => {
     const transactionsOpeningDay = isNumber(sale.transactionsOpeningDay) ? sale.transactionsOpeningDay : (sale.transactionsOpeningDay.trim() === '' ? '0' : 'NaN')
     const transactionTotal = isNumber(sale.transactionTotal) ? sale.transactionTotal : (sale.transactionTotal.trim() === '' ? '0' : 'NaN')
 
-    const totalTrueLegacyFee = parseFloat(sale.minimumActual) + parseFloat(sale.commissionRate)
-    const netShareToClient = parseFloat(sale.grossSalesActualClover) - totalTrueLegacyFee
     const grossSalesEntireSale = parseFloat(sale.grossSalesCreditDebit || sale.grossSalesCash)
+    const grossSalesCash = parseFloat(sale.grossSalesCash)
+    const cashOutsideClover = parseFloat(sale.cashOutsideClover)
+    const grossSalesCreditDebit = parseFloat(sale.grossSalesCreditDebit)
+    const grossSalesActualClover = parseFloat(sale.grossSalesActualClover)
+    const grossProceeds = grossSalesCash + cashOutsideClover + (grossSalesCreditDebit * 0.965) - (grossSalesActualClover * 0.0775)
+    const commissionRateTotal = parseFloat(`${sale.commissionRate/100 * sale.grossSalesActualClover}`)
+    const trueLegacyFeePercent = parseFloat(`${sale.commissionRate/100}`)
+    const trueLegacyFeeMinimum = parseFloat(sale.trueLegacyFeeMinimum)
+    const trueLegacyFee = (grossProceeds - trueLegacyFeeMinimum) * trueLegacyFeePercent
+    const totalTrueLegacyFee = trueLegacyFeeMinimum + trueLegacyFee
+    const netShareToClient = parseFloat(sale.grossSalesActualClover) - totalTrueLegacyFee
+    
 
     return {
         ...sale,
@@ -26,10 +36,11 @@ export const saleToInvoice = sale => {
         MailingAddress: ["clientMailingAddress1", "clientMailingAddress2", "clientMailingCity"
             , "clientMailingState", "clientPostalCode"].reduce((acc, cur) => sale[cur] && sale[cur] !== '0' ? `${acc}, ${sale[cur]}` : acc, '')
             .substring(1).trim(),
-        grossProceeds: toCurrency(sale.grossSalesActualClover),
+        grossProceeds: toCurrency(grossProceeds).toLocaleString("en-US"),
         trueLegacyFeeMinimum: toCurrency(sale.minimumActual),
-        trueLegacyFee: toCurrency(sale.commissionRate),
-        totalTrueLegacyFee: toCurrency(`${totalTrueLegacyFee}`),
+        // trueLegacyFee: toCurrency(`${sale.commissionRate/100 * sale.grossSalesActualClover}`),
+        trueLegacyFee: trueLegacyFeePercent,
+        totalTrueLegacyFee: toCurrency(totalTrueLegacyFee),
         netShareToClient: toCurrency(`${netShareToClient}`),
         disposal: toCurrency(sale.disposalFee),
         totalAmountDue: toCurrency(`${netShareToClient - parseFloat(sale.disposalFee)}`),
@@ -42,5 +53,6 @@ export const saleToInvoice = sale => {
 
         grossSalesEntireSale: toCurrency(`${grossSalesEntireSale}`),
         avePurchaseAmountEntireSale: toCurrency(`${grossSalesEntireSale / parseInt(transactionTotal)}`),
+
     }
 }
--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -8,4 +8,33 @@
    }
  }
 
- 
+ function changeContentNew() {
+  // document.getElementById("landing-card-title").innerHTML="New Sale";
+  document.getElementById("landing-card-btn").href = "/sale";
+  document.getElementById("landing-card-text").innerHTML="Add data for a brand new estate sale"
+  document.getElementById("landing-card-btn").innerHTML="Add New Sale now"
+  document.getElementById("tab-new").className="nav-link active";
+  document.getElementById("tab-existing").className="nav-link";
+  document.getElementById("tab-admin").className="nav-link";
+}
+
+function changeContentExisting() {
+  // document.getElementById("landing-card-title").innerHTML="Existing Sales";
+  document.getElementById("landing-card-btn").href = "/sales";
+  document.getElementById("landing-card-text").innerHTML="View a list of your existing estate sales"
+  document.getElementById("landing-card-btn").innerHTML="View Sales now";
+  document.getElementById("tab-new").className="nav-link";
+  document.getElementById("tab-existing").className="nav-link active";
+  document.getElementById("tab-admin").className="nav-link";
+}
+
+function changeContentAdmin() {
+  // document.getElementById("landing-card-title").innerHTML="Admin Section";
+  document.getElementById("landing-card-btn").href = "/admin";
+  document.getElementById("landing-card-text").innerHTML="View and manage the data that fills the dropdown selects in your data entry form"
+  document.getElementById("landing-card-btn").innerHTML="View Admin now";
+  document.getElementById("tab-new").className="nav-link";
+  document.getElementById("tab-existing").className="nav-link";
+  document.getElementById("tab-admin").className="nav-link active";
+}
+
